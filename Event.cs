7bc6f5de1e31d361bce4fd7db27e40aede70957d
Alexandr Kurdyukov <alexandr_kurdyukov@protonmail.com>
2019-05-14 17:49:02 +0300

    s

--- Event.cs
+++ Event.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace WatcherAPI
 {
     public class Event
     {
         [Key]
+        [JsonProperty("id")]
         public string id { get; set; }
+        [JsonProperty("latitude")]
         public string latitude { get; set; }
+        [JsonProperty("longitude")]
         public string longitude { get; set; }
+        [JsonProperty("name")]
         public string name { get; set; }
+        [JsonProperty("description")]
         public string description { get; set; }
+        [JsonProperty("type")]
         public int type { get; set; }
     }
 }
